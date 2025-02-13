404825b76ebdbaae8fe76b5d576064d94c7d0b48
Danild Zambrana <42362162+DanildZambrana@users.noreply.github.com>
2025-02-13 15:37:09 -0600

    refactor: Enhance customers page with detailed cards
    The customers page has been updated to include detailed cards rather than simple text. New components, namely 'CardWithButtons' and 'OutlineButtonComponent', have been imported and implemented. The cards display customer information like the name, email, total orders, and amount spent, along with 'Edit' and 'Delete' buttons for each customer. This change not only makes the page more informative but also improves the user experience by making the interface more interactive.

--- lib/screens/customers.dart
+++ lib/screens/customers.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:sales_app/components/card_with_buttons_component.dart';
 import 'package:sales_app/structure/navbar.dart';
+import 'package:sales_app/components/outline_button_component.dart';
 
 class CustomersPage extends StatelessWidget {
   const CustomersPage({super.key});
@@ -7,12 +9,66 @@ class CustomersPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Customers"),
-      ),
-      body: const Center(
-        child: Text("Customers page"),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("Customers"),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              CardWithButtons(
+                  cardTitle: "John Doe",
+                  cardSubtitle: "jhon@email.com",
+                  startValue: "Orders: 5",
+                  endValue: "Spent: \$299.95",
+                  actionButtons: [
+                    OutlineButtonComponent(
+                        text: "Edit",
+                        onPressed: () {},
+                        color: Colors.deepPurple),
+                    OutlineButtonComponent(
+                        text: "Delete", onPressed: () {}, color: Colors.red),
+                  ]),
+              CardWithButtons(
+                  cardTitle: "John Doe",
+                  cardSubtitle: "jhon@email.com",
+                  startValue: "Orders: 5",
+                  endValue: "Spent: \$299.95",
+                  actionButtons: [
+                    OutlineButtonComponent(
+                        text: "Edit",
+                        onPressed: () {},
+                        color: Colors.deepPurple),
+                    OutlineButtonComponent(
+                        text: "Delete", onPressed: () {}, color: Colors.red),
+                  ]),
+              CardWithButtons(
+                  cardTitle: "John Doe",
+                  cardSubtitle: "jhon@email.com",
+                  startValue: "Orders: 5",
+                  endValue: "Spent: \$299.95",
+                  actionButtons: [
+                    OutlineButtonComponent(
+                        text: "Edit",
+                        onPressed: () {},
+                        color: Colors.deepPurple),
+                    OutlineButtonComponent(
+                        text: "Delete", onPressed: () {}, color: Colors.red),
+                  ]),
+              CardWithButtons(
+                  cardTitle: "John Doe",
+                  cardSubtitle: "jhon@email.com",
+                  startValue: "Orders: 5",
+                  endValue: "Spent: \$299.95",
+                  actionButtons: [
+                    OutlineButtonComponent(
+                        text: "Edit",
+                        onPressed: () {},
+                        color: Colors.deepPurple),
+                    OutlineButtonComponent(
+                        text: "Delete", onPressed: () {}, color: Colors.red),
+                  ]),
+            ],
+          ),
+        ));
   }
 }
